93d88a996d485b517aefc2a0964f6fe101fac21d
BrenZorn <tacknsmack1516@gmail.com>
2025-01-13 23:12:53 -0800

    currentUser route added
    this will get all the info related to the current user

--- Routes/Authentication.js
+++ Routes/Authentication.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const jwt = require('jsonwebtoken');
 const router = express.Router()
+
 const { 
     getAuth, 
     createUserWithEmailAndPassword, 
@@ -9,7 +11,7 @@ const {
     sendPasswordResetEmail
 } = require('../Lib/firebase');
 const { addUser, getUser } = require('../Lib/mongo');
-const jwt = require('jsonwebtoken');
+const {authenticateToken} = require('../Middleware/jwtVerification')
 
 const auth = getAuth();
 
@@ -66,7 +68,7 @@ router.post('/signin', async (req, res)=>{
 })
 
 
-router.post('/signout', (req, res)=>{
+router.post('/signout', authenticateToken,(req, res)=>{
   signOut(auth).then(() => {
     console.log('signed out')
   }).catch((error) => {
--- Routes/Homepage.js
+++ Routes/Homepage.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const { authenticateToken } = require("../Middleware/jwtVerification")
+const { getUser } = require('../Lib/mongo')
 
 const router = express.Router()
 
 //test route
 router.get('/home', authenticateToken, (req, res)=>{
     res.send('authenticated').status(200)
+})
+
+router.get('/currentUser', authenticateToken, async (req, res)=>{
+    if(req.user){
+        try{
+            let user = await getUser(req.email)
+            res.json(
+                {
+                    user:{
+                        "UserName": user.UserName,
+                        "Email": user.Email
+                    }
+            }).status(200)
+        }catch(err){
+            res.json({"error":"No user found"}).status(500)
+        }
+        
+    }
+    else{
+        res.json({"error":"No user found"}).status(500)
+    }
 })
 
 module.exports = router
