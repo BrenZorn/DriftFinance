e1f66840fbc6885ae3cd26c1d028c94398d87931
BrenZorn <tacknsmack1516@gmail.com>
2025-01-18 20:11:08 -0800

    removed merge conflict

--- Routes/Homepage.js
+++ Routes/Homepage.js
@@ -9,34 +9,12 @@ router.get('/home', authenticateToken, (req, res)=>{
     res.send('authenticated').status(200)
 })
 
-<<<<<<< HEAD
-router.get('/currentUser', authenticateToken, async (req, res)=>{
-    if(req.user){
-        try{
-            let user = await getUser(req.email)
-            res.json(
-                {
-                    user:{
-                        "UserName": user.UserName,
-                        "Email": user.Email
-                    }
-            }).status(200)
-        }catch(err){
-            res.json({"error":"No user found"}).status(500)
-        }
-        
-    }
-    else{
-        res.json({"error":"No user found"}).status(500)
-    }
-=======
 router.get('/currentUser', authenticateToken,(req, res)=>{
      getUser(req.user.email)
      .then((user)=>{
         //edit data to only send back what is needed 
         res.json(user).status(200)
      })
->>>>>>> JWT
 })
 
 module.exports = router
